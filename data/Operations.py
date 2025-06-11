0f95ae1d9aa0142219395f38101bcad1c7869b13
Carson Nitta <nittac@oregonstate.edu>
2025-06-10 22:00:31 -0700

    made dictionary for operations

---
+++ data/Operations.py
@@ -0,0 +1,89 @@
+operations = {
+    'add': 2,
+    'subtract': 2,
+    'multiply': 2,
+    'divide': 2,
+    'log': 1,
+    'sqrt': 1,
+    'factorial': 1,
+    'gcd': 2,
+    'lcm': 2,
+    'power': 2,
+    'max': 2,
+    'min': 2,
+    'reminder': 2,
+    'negate': 1,
+    'inverse': 1,
+    'floor': 1,
+    'sine': 1,
+    'cosine': 1,
+    'tangent': 1,
+    'circle_area': 1,
+    'circumface': 1,
+    'rectangle_perimeter': 2,
+    'rectangle_area': 2,
+    'square_perimeter': 1,
+    'square_area': 1,
+    'rhombus_perimeter': 1,
+    'rhombus_area': 2,
+    'quadrilateral_area': 3,
+    'volume_cone': 2,
+    'volume_rectangular_prism': 3,
+    'volume_cube': 1,
+    'volume_sphere': 1,
+    'volume_cylinder': 2,
+    'surface_cylinder': 2,
+    'surface_cube': 1,
+    'surface_rectangular_prism': 3,
+    'surface_sphere': 1,
+    'cube_edge_by_volume': 1,
+    'diagonal': 2,
+    'square_edge_by_perimeter': 1,
+    'square_edge_by_area': 1,
+    'triangle_perimeter': 3,
+    'triangle_area': 2,
+    'triangle_area_three_edges': 3,
+    'negate_prob': 1,
+    'permutation': 2,
+    'p_after_gain': 2,
+    'original_price_before_gain': 2,
+    'original_price_before_loss': 2,
+    'speed': 2,
+    'speed_in_still_water': 2,
+    'stream_speed': 2,
+    'choose': 2,
+}
+
+"""
+
+Removed operations:
+
+round
+radians_to_degree
+degree_to_radians
+sum_consecutive_number
+circle_arc
+semi_circle_perimiter
+circle_sector_area
+trapezium_area
+surface_cone
+side_by_diagonal
+union_prob
+combination (dataset uses "choose" instead???)
+count_interval
+percent
+p_after_loss
+price_after_gain
+price_after_loss
+from_percent
+gain_percent
+loss_percent
+negate_percent
+to_percent
+combined_work
+find_work
+speed_ratio_steel_to_stream
+
+
+
+"""
